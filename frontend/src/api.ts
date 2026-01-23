4ef6de67a1290bced29bf87dd4c8d4449eed35d3
Nick Satsuro <satsurono@yandex.ru>
2026-01-23 23:57:11 +0300

    first wotking version (only front and back)

---
+++ frontend/src/api.ts
@@ -0,0 +1,13 @@
+import { type Term, type GraphData } from './types';
+
+const API_URL = 'http://localhost:8000/api';
+
+export const fetchTerms = async (): Promise<Term[]> => {
+  const res = await fetch(`${API_URL}/terms`);
+  return res.json();
+};
+
+export const fetchGraphData = async (): Promise<GraphData> => {
+  const res = await fetch(`${API_URL}/graph`);
+  return res.json();
+};
